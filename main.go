e157a7e4727a240ad1e0d273f36eabbbc84788dc
Cristi <cristi.duluta@gmail.com>
2022-09-24 13:46:26 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;  charset=UTF-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html;  charset=UTF-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:user@mail.com\">user@mail.com</a>.")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", pathHandler)
+	http.HandleFunc("/contact", contactHandler)
+	fmt.Println("Starting the server on: 3000...")
+	http.ListenAndServe(":3000", nil)
+}
