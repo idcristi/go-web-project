707e8cd6480f6796f450a103747d2ac2c837b777
Cristi <cristi.duluta@gmail.com>
2022-09-29 18:11:33 +0300

    Added users create method

--- main.go
+++ main.go
@@ -5,44 +5,32 @@ import (
 	"go-web-dev/controllers"
 	"go-web-dev/templates"
 	"go-web-dev/views"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
-
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	t, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, nil)
-}
 
 func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
 		templates.FS,
-		"home.gohtml", "tailwind.gohtml"))))
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(
-		templates.FS,
-		"contact.gohtml", "tailwind.gohtml"))))
-	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(
-		templates.FS,
-		"faq.gohtml", "tailwind.gohtml"))))
+		"home.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+	r.Get("/faq", controllers.FAQ(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
+
 	var usersC controllers.Users
 	usersC.Templates.New = views.Must(views.ParseFS(
-		templates.FS,
-		"signup.gohtml", "tailwind.gohtml"))
+		templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	r.Get("/signup", usersC.New)
+	r.Post("/signup", usersC.Create)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 	fmt.Println("Starting the server on :3000...")
-
 	http.ListenAndServe(":3000", r)
 }
